fb6e6c070eb36b39ed888931c26b445cfe7e4b04
huzaifa250 <huz.dark1@gmail.com>
2024-12-25 15:54:04 +0200

    repo

---
+++ __manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "NESCO",
+
+    'summary': "Coffee Shop Management System",
+
+    'description': """
+It's web application
+    """,
+
+    'author': "Huzaifa Elnaeem",
+    'website': "https://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'website', 'sale_management', 'stock', 'website_sale'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/nesco_coffee_shop_temp.xml',
+        'views/create_submit_product_temp.xml',
+        'views/views.xml',
+        # 'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ controllers/controllers.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+from odoo.http import request
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class CoffeeShop(http.Controller):
+    @http.route('/products', auth='public', website=True)
+    def list_products(self, **kwargs):
+        # get all the products from the product table
+        products = request.env['product.template'].sudo().search([])
+        # Check if no products are found
+        if not products:
+            # Render a template for no products or pass a message to the existing template
+            return request.render('nesco.coffee_shop_temp', {
+                'my_products': [],
+                'error_message': 'No products available at the moment.'
+            })
+
+        # if products are found pass data to the template
+        return request.render('nesco.coffee_shop_temp', {
+            'my_products': products,
+            'error_message': False  # No error, so no message
+        })
+
+    # Route for new product creation form page
+    @http.route('/new/product', type='http', auth='public', website=True, csrf=True)
+    def create_product_form(self, **kwargs):
+        return request.render("nesco.create_submit_product_temp")
+
+    # Route to handle form submission for creating a new product
+    @http.route('/submit/product', auth='public', website=True, csrf=True)
+    def submit_product(self, **kwargs):
+        # allow users to create product
+        name = kwargs.get('name')  # product name
+        # deatiled_type = kwargs.get('deatiled_type')  # optional
+        # Log the received data
+        _logger.info(
+            f"Received data: name={name}")
+        product = request.env['product.template'].sudo().create(
+            {'name': name,
+             # 'deatiled_type': deatiled_type or '',
+             })
+        # print("Name is *************** ", name)
+        _logger.info(f"product created: {product}")
+        # _logger.error(f"Error creating product:")
+        # Redirect or render a success page after creation
+        return request.redirect('/products')
